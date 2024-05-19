066d09da6633eb238bf347ff6f21b54b3b285001
Rachel H. <rachel.heneault@gmail.com>
2024-05-18 21:08:23 -0400

    i18n

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 
-], function (Controller, MessageToast, JSONModel) {
+], function (Controller, MessageToast, JSONModel,ResourceModel) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough",{
         onInit : function () {
@@ -14,10 +15,20 @@ sap.ui.define([
              }   
              var oModel = new JSONModel(oData);
              this.getView().setModel(oModel);  
+             // set i18n model 
+             var i18nModel = new ResourceModel({
+                bundleName : "sap.ui.demo.walkthrough.i18n.i18n",
+                supportLocales : [""],
+                fallbackLocales : ""
+             })
+             this.getView().setModel(i18nModel,"i18n")
         }, 
         onShowHello : function  () {
- 
-                MessageToast.show("Hello There")
+                // read msg from i18n model!
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                var oRecipient = this.getView().getModel().getProperty("/recipient/name");
+                var sMsg = oBundle.getText("helloMsg", [oRecipient]);
+                MessageToast.show(sMsg);
         }
     })
 
